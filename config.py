764278476ed7ce08f7227cddcaf4bdb9fc00a4aa
Arpit <arpitsengar99@gmail.com>
2023-10-08 10:16:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,47 @@
+import os 
+import streamlit as st
+
+# NEWS_API_KEY = st.streamlit["NEWS_API"]
+NEWS_API_KEY = os.getenv("NEWS_API")
+KEYWORD = "alzheimer"
+
+APOE_CATEGORIES = ['APOE Genotype_2,2', 'APOE Genotype_2,3', 'APOE Genotype_2,4', 'APOE Genotype_3,3',
+                   'APOE Genotype_3,4', 'APOE Genotype_4,4']
+PTHETHCAT_CATEGORIES = ['PTETHCAT_Hisp/Latino', 'PTETHCAT_Not Hisp/Latino', 'PTETHCAT_Unknown']
+IMPUTED_CATEGORIES = ['imputed_genotype_True', 'imputed_genotype_False']
+PTRACCAT_CATEGORIES = ['PTRACCAT_Asian', 'PTRACCAT_Black', 'PTRACCAT_White']
+PTGENDER_CATEGORIES = ['PTGENDER_Female', 'PTGENDER_Male']
+APOE4_CATEGORIES = ['APOE4_0', 'APOE4_1', 'APOE4_2']
+
+CSS = open("assets/css/styles.css", 'r').read()
+
+BACKGROUND = "assets/images/bg.webp"
+BANNER = "assets/images/banner.webp"
+DEFAULT_IMAGE = "assets/images/default.webp"
+SIDE_BANNER = "assets/images/side_banner.webp"
+EMOJI = "assets/images/emo.webp"
+
+TEAM_MEMBERS = [
+    {"name": "Arpit Sengar", "role": "Developer", "links":["https://www.linkedin.com/in/arpitsengar", "https://github.com/arpy8"]},
+    {"name": "Aditya Bhardwaj", "role": "Developer", "links":["https://www.linkedin.com/in/aditya-bhardwaj-3a6437232/", "https://github.com/adityabhardwajjj"]},
+    {"name": "Harshit Jain", "role": "Developer", "links":["https://www.linkedin.com/in/harshitjainnn/", "https://github.com/HarshitJainn"]},
+    {"name": "Siddharth Mohril", "role": "Developer", "links":["https://www.linkedin.com/in/siddharth-mohril-361678250/", "https://github.com/siddharth-mohril"]},
+    {"name": "Aditya Jain", "role": "Developer", "links":["https://www.linkedin.com/in/chimpyaj/", "https://github.com/ajhasbeensummoned"]},
+]
+
+ABBREVIATION = {
+                "AD": "Alzheimer's Disease ",
+                "LMCI": "Late Mild Cognitive Impairment ",
+                "CN": "Cognitively Normal"
+            }
+
+CONDITION_DESCRIPTION = {
+    "AD": "This indicates that the individual's data aligns with characteristics commonly associated with "
+        "Alzheimer's disease. Alzheimer's disease is a progressive neurodegenerative disorder that affects "
+        "memory and cognitive functions.",
+    "LMCI": "This suggests that the individual is in a stage of mild cognitive impairment that is progressing "
+            "towards Alzheimer's disease. Mild Cognitive Impairment is a transitional state between normal "
+            "cognitive changes of aging a   nd more significant cognitive decline.",
+    "CN": "This suggests that the individual has normal cognitive functioning without significant impairments. "
+        "This group serves as a control for comparison in Alzheimer's research."
+}
