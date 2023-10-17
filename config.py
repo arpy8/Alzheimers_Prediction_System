97259d0f66760906f2aee552acba0c19a1d5debf
Arpit <arpitsengar99@gmail.com>
2023-10-17 18:02:49 +0530

    refactor: rearranged variable names according to page

--- config.py
+++ config.py
@@ -1,33 +1,23 @@
-import os 
 import streamlit as st
 
-NEWS_API_KEY = st.secrets["NEWS_API"]
-KEYWORD = "alzheimer"
-
-APOE_CATEGORIES = ['APOE Genotype_2,2', 'APOE Genotype_2,3', 'APOE Genotype_2,4', 'APOE Genotype_3,3',
-                   'APOE Genotype_3,4', 'APOE Genotype_4,4']
-PTHETHCAT_CATEGORIES = ['PTETHCAT_Hisp/Latino', 'PTETHCAT_Not Hisp/Latino', 'PTETHCAT_Unknown']
-IMPUTED_CATEGORIES = ['imputed_genotype_True', 'imputed_genotype_False']
-PTRACCAT_CATEGORIES = ['PTRACCAT_Asian', 'PTRACCAT_Black', 'PTRACCAT_White']
-PTGENDER_CATEGORIES = ['PTGENDER_Female', 'PTGENDER_Male']
-APOE4_CATEGORIES = ['APOE4_0', 'APOE4_1', 'APOE4_2']
-
+# PAGE CONFIG
 CSS = open("assets/css/styles.css", 'r').read()
 
+# ASSETS
 BACKGROUND = "assets/images/bg.webp"
 BANNER = "assets/images/banner.webp"
 DEFAULT_IMAGE = "assets/images/default.webp"
 SIDE_BANNER = "assets/images/side_banner.webp"
 EMOJI = "assets/images/emo.webp"
 
-TEAM_MEMBERS = [
-    {"name": "Arpit Sengar", "role": "Developer", "links":["https://www.linkedin.com/in/arpitsengar", "https://github.com/arpy8"]},
-    {"name": "Aditya Bhardwaj", "role": "Developer", "links":["https://www.linkedin.com/in/aditya-bhardwaj-3a6437232/", "https://github.com/adityabhardwajjj"]},
-    {"name": "Harshit Jain", "role": "Developer", "links":["https://www.linkedin.com/in/harshitjainnn/", "https://github.com/HarshitJainn"]},
-    {"name": "Siddharth Mohril", "role": "Developer", "links":["https://www.linkedin.com/in/siddharth-mohril-361678250/", "https://github.com/siddharth-mohril"]},
-    {"name": "Aditya Jain", "role": "Developer", "links":["https://www.linkedin.com/in/chimpyaj/", "https://github.com/ajhasbeensummoned"]},
-]
-
+# PREDICTION PAGE
+APOE_CATEGORIES = ['APOE Genotype_2,2', 'APOE Genotype_2,3', 'APOE Genotype_2,4', 
+                   'APOE Genotype_3,3', 'APOE Genotype_3,4', 'APOE Genotype_4,4']
+PTHETHCAT_CATEGORIES = ['PTETHCAT_Hisp/Latino', 'PTETHCAT_Not Hisp/Latino', 'PTETHCAT_Unknown']
+IMPUTED_CATEGORIES = ['imputed_genotype_True', 'imputed_genotype_False']
+PTRACCAT_CATEGORIES = ['PTRACCAT_Asian', 'PTRACCAT_Black', 'PTRACCAT_White']
+PTGENDER_CATEGORIES = ['PTGENDER_Female', 'PTGENDER_Male']
+APOE4_CATEGORIES = ['APOE4_0', 'APOE4_1', 'APOE4_2']
 ABBREVIATION = {
                 "AD": "Alzheimer's Disease ",
                 "LMCI": "Late Mild Cognitive Impairment ",
@@ -44,3 +34,22 @@ CONDITION_DESCRIPTION = {
     "CN": "This suggests that the individual has normal cognitive functioning without significant impairments. "
         "This group serves as a control for comparison in Alzheimer's research."
 }
+
+# NEWS PAGE
+NEWS_API_KEY = st.secrets["NEWS_API"]
+KEYWORD = "alzheimer"
+
+
+# CHATBOT PAGE
+HF_EMAIL = st.secrets['HF_GMAIL']
+HF_PASS = st.secrets['HF_PASS']    
+
+
+# TEAM MEMBERS PAGE
+TEAM_MEMBERS = [
+    {"name": "Arpit Sengar", "role": "Developer", "links":["https://www.linkedin.com/in/arpitsengar", "https://github.com/arpy8"]},
+    {"name": "Aditya Bhardwaj", "role": "Developer", "links":["https://www.linkedin.com/in/aditya-bhardwaj-3a6437232/", "https://github.com/adityabhardwajjj"]},
+    {"name": "Harshit Jain", "role": "Developer", "links":["https://www.linkedin.com/in/harshitjainnn/", "https://github.com/HarshitJainn"]},
+    {"name": "Siddharth Mohril", "role": "Developer", "links":["https://www.linkedin.com/in/siddharth-mohril-361678250/", "https://github.com/siddharth-mohril"]},
+    {"name": "Aditya Jain", "role": "Developer", "links":["https://www.linkedin.com/in/chimpyaj/", "https://github.com/ajhasbeensummoned"]},
+]
